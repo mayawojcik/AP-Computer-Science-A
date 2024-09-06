d9dbb2c616f13d6cc2f6a5c4b70a79a0234fd5e5
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-09-06 11:08:45 -0500

    9-5-24

--- src/Q1/Prog58b.java
+++ src/Q1/Prog58b.java
@@ -5,5 +5,24 @@ public class Prog58b {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter first number: ");
+        int A = input.nextInt();
+        System.out.println();
+
+        System.out.print("Enter second number: ");
+        int B = input.nextInt();
+        System.out.println();
+
+        System.out.print("Enter third number: ");
+        int C = input.nextInt();
+        System.out.println();
+
+        // positive root
+        int pRoot = (-B + (int)Math.pow(B, 2) - (4 * A * C)) / (2 * A);
+
+        // negative root
+        int nRoot = (-B - (int)Math.pow(B, 2) - (4 * A * C)) / (2 * A);
+        System.out.println("Roots: " + pRoot + "," + nRoot);
+        //add square root
     }
 }

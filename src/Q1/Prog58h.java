0900c97039153a7b9913ec38aa414894973443bf
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-09-06 11:25:17 -0500

    9.6.24

--- src/Q1/Prog58h.java
+++ src/Q1/Prog58h.java
@@ -5,6 +5,24 @@ public class Prog58h {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
 
+        System.out.print("Enter interest rate: ");
+        int R = input.nextInt();
+        System.out.println();
+
+        System.out.print("Enter time compounded: ");
+        int N = input.nextInt();
+        System.out.println();
+
+        System.out.print("Enter days to be kept: ");
+        int T = input.nextInt();
+        System.out.println();
+
+        System.out.print("Enter amount saved: ");
+        int P = input.nextInt();
+        System.out.println();
+
+        double amount = P * ((1 + (int)Math.pow(((0.01 * R)/N),(N * T)/365)));
+
 
     }
 }

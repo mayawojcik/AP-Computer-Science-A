65472db23f0cbc8dc02b5f21c7af366587ecc672
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-09-09 11:04:44 -0500

    9.9.24

--- src/Q1/Prog58b.java
+++ src/Q1/Prog58b.java
@@ -18,11 +18,20 @@ public class Prog58b {
         System.out.println();
 
         // positive root
-        int pRoot = (-B + (int)Math.pow(B, 2) - (4 * A * C)) / (2 * A);
+        int pRoot = (-B + (int)Math.pow((int)Math.pow(B, 2) - (4 * A * C), 0.5)) / (2 * A);
 
         // negative root
-        int nRoot = (-B - (int)Math.pow(B, 2) - (4 * A * C)) / (2 * A);
+        int nRoot = (-B - (int)Math.pow((int)Math.pow(B, 2) - (4 * A * C), 0.5)) / (2 * A);
         System.out.println("Roots: " + pRoot + "," + nRoot);
         //add square root
     }
 }
+/*
+Enter first number: 1
+
+Enter second number: 5
+
+Enter third number: 6
+
+Roots: -2,-3
+ */

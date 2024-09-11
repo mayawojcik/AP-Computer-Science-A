d581cd125d6ba9f1f17cb9be6900ef01ea6e675b
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-09-11 11:19:55 -0500

    9.11.24

--- src/Q1/porg88a.java
+++ src/Q1/porg88a.java
@@ -10,9 +10,13 @@ public class porg88a {
         // sum difference average product max min etc
         int sum = num1 + num2;
         int dif = num1 - num2;
-        // TODO: product and average
+        // TODO: product and average //(done)
+        int prod = num1 * num2;
+        int ave = sum / 2;
         int abs = Math.abs(dif); //absolute value
         int max, min;
+
+
 
         // the code inside 'if' will only run if the condition is true
         if (num1 > num2) {
@@ -30,9 +34,24 @@ public class porg88a {
             min = num1;
         }
 
-        System.out.println("Original numbers are " + num1 + "and" + num2);
+        System.out.println("Original numbers are " + num1 + " and " + num2);
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + dif);
+        System.out.println("Product = " + prod);
+        System.out.println("Average = " + ave);
+        System.out.println("Absolute value = " + abs);
         // TODO: print the rest
         System.out.println("Maximum = " + max);
         System.out.println("Minimum = " + min);
     }
 }
+/*
+Original numbers are 10 and 12
+Sum = 22
+Difference = -2
+Product = 120
+Average = 11
+Absolute value = 2
+Maximum = 12
+Minimum = 10
+ */

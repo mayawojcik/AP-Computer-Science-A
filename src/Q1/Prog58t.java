edf15f9c07353feb9eced3a4b3668be0c55a723b
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-09-16 11:21:41 -0500

    9,16,24

--- src/Q1/Prog58t.java
+++ src/Q1/Prog58t.java
@@ -13,8 +13,25 @@ public class Prog58t {
         double change = amountR - price;
         System.out.printf("Change due: = %.2f\n", change);
 
-        double dollars = change / 1.00;
-        System.out.printf("Dollars: = %.2f\n", dollars);
+        double dollars = (change / 1.0) - 0.68;
+        System.out.printf("Dollars: = %.0f\n", dollars);
+
+        double change2 = (change - dollars);
+        double quarters = ((change2 / 0.25) - 0.72);
+        System.out.printf("Quarters: = %.0f\n", quarters);
+
+        double change3 = (change2 - (quarters * 0.25));
+        double dimes = (change3 / 0.10) - 0.80;
+        System.out.printf("Dimes: = %.0f\n", dimes);
+
+        double change4 = (change3 - (dimes * 0.10));
+        double nickels = (change4 / 0.05) - 0.60;
+        System.out.printf("Nickels: = %.0f\n", nickels);
+
+        double change5 = (change4 - (nickels * 0.05));
+        double pennies = (change5 / 0.01);
+        System.out.printf("Pennies: = %.0f\n", pennies);
+
 
 
 

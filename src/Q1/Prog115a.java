654612750302a1761ca683103fa476167ef32ed7
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-09-16 10:47:30 -0500

    9.16.24

---
+++ src/Q1/Prog115a.java
@@ -0,0 +1,12 @@
+package Q1;
+import java.util.*;
+
+public class Prog115a {
+    public static void main(String[] args) {
+        int lcv = 0; // lcv = loop control variable
+        while (lcv <= 34) {
+            lcv += 2; // lcv = lcv + 2;
+            System.out.println(lcv);
+        }
+    }
+}
--- src/Q1/Prog76a.java
+++ src/Q1/Prog76a.java
@@ -5,11 +5,15 @@ public class Prog76a {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter digit between 1-9");
+        System.out.print("Enter digit between 1-9: ");
         int num = input.nextInt();
 
         int product = num * 9;
-        int output =
+        int output = product * 12345679;
+
+        System.out.print(num + "X" + "9");
+
+        System.out.print(output);
 
     }
 }

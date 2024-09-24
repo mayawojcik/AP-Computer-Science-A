a029602db1af181eda87636c5168e1c41ec33440
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-09-24 11:25:08 -0500

    9.24.24

---
+++ src/Q1/Prog72t.java
@@ -0,0 +1,18 @@
+package Q1;
+import java.util.*;
+
+public class Prog72t {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first time: ");
+        int time1 = input.nextInt();
+        System.out.print("Enter the second time: ");
+        int time2 = input.nextInt();
+        int time = Math.abs(time2 - time1);
+        System.out.print
+
+
+
+    }
+}

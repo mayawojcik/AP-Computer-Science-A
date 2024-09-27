5471e7ab8718ccb332f5d66430197f74d18b62e0
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-09-27 10:56:16 -0500

    9.27.24

--- src/Q1/prog166f.java
+++ src/Q1/prog166f.java
@@ -5,8 +5,8 @@ public class prog166f {
     public static void main(String[] args) {
         int lcv = 0;
         while (lcv <= 10) {
-            lcv = 1;
-            System.out.println(lcv);
+            lcv += 1;
+            System.out.printf("%d", lcv);
         }
 
     }

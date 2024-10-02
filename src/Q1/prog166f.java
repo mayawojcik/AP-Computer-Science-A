752466e4a7c0ad61632b453045168b76038800fe
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-10-02 11:05:02 -0500

    02.10.24

--- src/Q1/prog166f.java
+++ src/Q1/prog166f.java
@@ -10,7 +10,29 @@ public class prog166f {
             lcv2 += lcv1;
 
 
-        }
 
+        }
+        System.out.println("The sum of the numbers 1-100: " + lcv2);
+        lcv2 += 101;
+        System.out.println("The sum of the numbers 1-101: " + lcv2);
+        lcv2 += 102;
+        System.out.println("The sum of the numbers 1-102: " + lcv2);
+        lcv2 += 103;
+        System.out.println("The sum of the numbers 1-103: " + lcv2);
+        lcv2 += 104;
+        System.out.println("The sum of the numbers 1-104: " + lcv2);
+        lcv2 += 105;
+        System.out.println("The sum of the numbers 1-105: " + lcv2);
+        lcv2 += 106;
+        System.out.println("The sum of the numbers 1-106: " + lcv2);
+        lcv2 += 107;
+        System.out.println("The sum of the numbers 1-107: " + lcv2);
+        lcv2 += 108;
+        System.out.println("The sum of the numbers 1-108: " + lcv2);
+        lcv2 += 109;
+        System.out.println("The sum of the numbers 1-109: " + lcv2);
+        lcv2 += 110;
+        System.out.println("The sum of the numbers 1-110: " + lcv2);
+        lcv2 += 111;
     }
 }

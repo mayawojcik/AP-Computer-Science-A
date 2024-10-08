7964ed3cca7571cf7a4931e2bb8889f011a8162e
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-10-08 11:24:55 -0500

    08.10.24

---
+++ src/Q1/LP43.java
@@ -0,0 +1,17 @@
+package Q1;
+import java.util.Scanner;
+
+public class LP43 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of eggs purchased: ");
+        int eggs = input.nextInt();
+
+        String price = " ";
+        if (eggs > 48) price = "$0.50 per dozen";
+        else if (eggs > 72) price = "$0.45 per dozen";
+        else if (eggs > 132) price = "0.40 per dozen";
+        else (eggs > )
+    }
+}
---
+++ src/Q1/LP46.java
@@ -0,0 +1,8 @@
+package Q1;
+import java.util.Scanner;
+
+public class LP46 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}

65a32b8d68b9ec60dbd0953893fe32c7df608040
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-10-15 11:17:09 -0500

    15.10.24

---
+++ src/Q1/prog285bcl/salesperson.java
@@ -0,0 +1,41 @@
+package Q1.prog285bcl;
+
+public class salesperson {
+    private int myId;
+    private int myCode;
+    private double mySales;
+    private double myComm;
+
+    private void calc() {
+        if (myCode == 5 || myCode == 8) {
+            if (mySales <= 5000)
+                myComm = getSales() * 0.075;
+            else
+                myComm = 5000 * 0.075 + (mySales - 5000) * 0.085;
+        } else if (myCode == 17) {
+            if (mySales <= 3500)
+                myComm =mySales * 0.095;
+            else
+                myComm = 3500 * 0.095 + (getSales() - 3500) * 0.12;
+        }
+    }
+
+    public salesperson(int id, int code, double sales) {
+        myId = id;
+        myCode = code;
+        mySales = sales;
+        myComm = 0;
+    }
+
+    public void setComm() { this.calc(); } //this. is optional
+
+    public int getId()       { return myId;   }
+    public int getCode()     { return myCode; }
+    public double getSales() { return mySales;}
+    public double getComm()  { return myComm; }
+
+    public String toString() {
+        return myId + "\t" + myCode + "\t\t" + mySales + "\t" + myComm;
+        // return getId() + "\t" + getCode() + ...
+    }
+}

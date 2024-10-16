82c185a0299cd8afb2f9f701995ff1c7edb1b78a
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-10-16 11:36:05 -0500

    16.10.24

--- src/Q1/LP49.java
+++ src/Q1/LP49.java
@@ -5,21 +5,22 @@ public class LP49 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number between 1 and 20: ");
-        int num = input.nextInt();
-        int comnum = (int)Math.random()*20+ 1;
+        double num = input.nextDouble();
+        double comnum = (double)Math.random()*20 +1;
+        double comnumr = Math.round(comnum * 1.0) / 1.0;
 
         String output = "";
-        if (num == comnum)  output = "You won!";
+        if (num == comnumr)  output = "You won!";
         else  output = "Better luck next time!";
 
-        System.out.println("Computer number: " + comnum);
+        System.out.println("Computer number: " + comnumr);
         System.out.println("Player's number: " + num);
         System.out.println(output);
     }
 }
 /*
-Enter a number between 1 and 20: 6
-Computer number: 0
-Player's number: 6
+Enter a number between 1 and 20: 12
+Computer number: 1.0
+Player's number: 12.0
 Better luck next time!
  */
--- src/Q1/LP514.java
+++ src/Q1/LP514.java
@@ -3,7 +3,15 @@ import java.util.*;
 
 public class LP514 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dice 1\t\tDice 2\t\tTotal");
+
+        double dice11 = (double)Math.random()*6 +1;
+        double dice1 = Math.round(dice11 * 1.0) / 1.0;
+        double dice22 = (double)Math.random()*6 +1;
+        double dice2 = Math.round(dice22 * 1.0) / 1.0;
+        double total = dice1 + dice2;
+        System.out.println(dice1 + "\t\t\t" + dice2 + "\t\t\t" + total);
 
     }
 }

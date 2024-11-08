0699ceaa64d2ffdcfb7d9a8b515dd94bec3792dd
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-11-08 10:57:17 -0600

    8.11.24

--- src/Q2/prog214b/CL214b.java
+++ src/Q2/prog214b/CL214b.java
@@ -2,14 +2,11 @@ package Q2.prog214b;
 
 public class CL214b {
     private int myNum;
-    private double myhours;
-    private double myrate;
-    private double myshiftFactor;
-    private double myyearToDate;
-    private double mygrossPay;
-    private double mywithholding;
-    private double myfica;
-    private double mynetPay;
+    private double myHours;
+    private double myBPR;
+    private double myRate;
+    private double myShiftFactor;
+
 
     public CL214b(int employeeNum) {
 

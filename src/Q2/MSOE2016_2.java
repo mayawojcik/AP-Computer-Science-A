60f058efd2c6a61ca8345db858855b0e39118c42
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-11-21 11:24:33 -0600

    21.11.24

--- src/Q2/MSOE2016_2.java
+++ src/Q2/MSOE2016_2.java
@@ -1,4 +1,16 @@
 package Q2;
+import java.util.*;
 
 public class MSOE2016_2 {
+    public static void prob2() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Length of side A?? ");
+        double a = input.nextDouble();
+        System.out.print("Length of side C?? ");
+        double c = input.nextDouble();
+
+        double C = Math.toRadians(c);
+        double A = Math.toRadians(a);
+    }
 }

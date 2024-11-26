012c9c36325a78a3a5ef2b1286912a4000c5d308
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-11-26 10:47:07 -0600

    26.11.24

--- src/Q2/MSOE2016_2.java
+++ src/Q2/MSOE2016_2.java
@@ -2,15 +2,29 @@ package Q2;
 import java.util.*;
 
 public class MSOE2016_2 {
-    public static void prob2() {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        System.out.print("Length of side A?? ");
+        System.out.println("What is side a? ");
         double a = input.nextDouble();
-        System.out.print("Length of side C?? ");
+        System.out.println("What is side c? ");
         double c = input.nextDouble();
-
-        double C = Math.toRadians(c);
-        double A = Math.toRadians(a);
+        System.out.println("Degree of angle a? ");
+        double alp = input.nextDouble();
+        double g = Math.toDegrees(Math.asin(c/a*Math.sin(Math.toRadians(alp)))); // Okay for competitors to assume asin argument <= 1
+        double g2 = 180-g;
+        if (alp+g < 180) {
+            System.out.printf("The two values for angle C: %g and %g.\n", g, g2);
+        } else {
+            System.out.printf("One value for angle C: %g.\n", g);
+        }
     }
 }
+/*
+What is side a?
+54
+What is side c?
+38
+Degree of angle a?
+164
+The two values for angle C: 11.1844 and 168.816.
+ */

33ce2fb335b8ce7bfbaa00b26ccd911478b4765a
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-12-03 11:24:32 -0600

    3.12.24

--- src/Q2/LP314cl/LP314.java
+++ src/Q2/LP314cl/LP314.java
@@ -1,4 +1,48 @@
 package Q2.LP314cl;
+import Q2.LP310.LP310cl;
+
+import java.util.*;
 
 public class LP314 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print  ("                Awbrey    Martinez");
+        System.out.println("\nNew Jersey:     ");
+        System.out.print  ("");
+        int AwNj = input.nextInt();
+        System.out.print  ("");
+        int MaNj = input.nextInt();
+        System.out.println("New York:       ");
+        System.out.print  ("");
+        int AwNy = input.nextInt();
+        System.out.print  ("");
+        int MaNy = input.nextInt();
+        System.out.println("Connecticut:    ");
+        System.out.print  ("");
+        int AwC = input.nextInt();
+        System.out.print  ("");
+        int MaC = input.nextInt();
+
+        LP314cl cat = new LP314cl(AwNj, MaNj, AwNy, MaNy, AwC, MaC);
+        cat.calc();
+
+        System.out.println(cat);
+    }
 }
+/*
+New Jersey:
+89008
+121032
+New York:
+314159
+271860
+Connecticut:
+213451
+231034
+ */
+
+
+
+
+
+
--- src/Q2/LP314cl/LP314cl.java
+++ src/Q2/LP314cl/LP314cl.java
@@ -6,37 +6,44 @@ public class LP314cl {
     private int AwNj;
     private int MaNj;
     private int AwC;
-    private int MaC
+    private int MaC;
+    private double totA;
+    private double totM;
     private int Aw;
     private int Ma;
     private int total;
 
     public LP314cl(int AwNy, int MaNy, int AwNj, int MaNj, int AwC, int MaC) {
-        AwNy = AwNy;
-        MaNy = MaNy;
-        AwNj = AwNj;
-        MaNj = MaNj;
-        AwC = AwC;
-        MaC = MaC;
+        this.AwNy = AwNy;
+        this.MaNy = MaNy;
+        this.AwNj = AwNj;
+        this.MaNj = MaNj;
+        this.AwC = AwC;
+        this.MaC = MaC;
+        totA = 0;
+        totM = 0;
         Aw = 0;
         Ma = 0;
         total = 0;
 
     }
     public void calc() {
-        Aw = AwNy + AwNj + AwC;
-        Ma = MaNy + MaNj + MaC;
+        Aw = this.AwNy + this.AwNj + this.AwC;
+        Ma = this.MaNy + this.MaNj + this.MaC;
         total = Aw + Ma;
+        totA = (Aw / total)*100;
+        totM = (Ma / total)*100;
     }
     public String toString() {
-        return "Election results for New York:\n" + "Awbrey: " + AwNy + "\n"
-                + "Martinez: " + MaNy + "\n\n" +
-                "Election results for New Jersey:\n" + "Awbrey: " + AwNj + "\n"
-                + "Martinez: " + MaNj + "\n\n" +
-                "Election results for Connecticut:\n" + "Awvrey: " + AwC + "\n"
-                + "Martinez: " + MaC + "\n\n" +
+        return "Election results for New York:\n" + "Awbrey: " + this.AwNy + "\n"
+                + "Martinez: " + this.MaNy + "\n\n" +
+                "Election results for New Jersey:\n" + "Awbrey: " + this.AwNj + "\n"
+                + "Martinez: " + this.MaNj + "\n\n" +
+                "Election results for Connecticut:\n" + "Awvrey: " + this.AwC + "\n"
+                + "Martinez: " + this.MaC + "\n\n" +
                 "Candidate              " + "Votes   " + "Percentage\n" +
-                "Awbrey                 " + Aw + "" + "\n" +
-                "Martinez               " + Ma + "" + "\n";
+                "Awbrey                 " + Aw + "   " + totA + "\n" +
+                "Martinez               " + Ma + "   " + totM + "\n" +
+                "TOTAL VOTES:           " + total;
     }
 }

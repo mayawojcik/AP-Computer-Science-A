135767187572f1656b7223e31b38cc428eb7ef6b
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-12-05 11:20:40 -0600

    05.12.24

---
+++ src/Q2/CL39/CL39.java
@@ -0,0 +1,22 @@
+package Q2.CL39;
+import java.util.*;
+
+public class CL39 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your birthdate!!: ");
+        System.out.println("Year!: ");
+        int year = input.nextInt();
+        System.out.println("Month!: ");
+        int month = input.nextInt();
+        System.out.println("Day!: ");
+        int day = input.nextInt();
+        System.out.println("Enter today's date!: ");
+        System.out.println("Year!: ");
+        int year2 = input.nextInt();
+        System.out.println("Month!: ");
+        int month2 = input.nextInt();
+        System.out.println("Day!: ");
+        int day2 = input.nextInt();
+    }
+}
---
+++ src/Q2/CL39/LP39cl.java
@@ -0,0 +1,28 @@
+package Q2.CL39;
+
+public class LP39cl {
+    private int year;
+    private int month;
+    private int day;
+    private int year2;
+    private int month2;
+    private int day2;
+    private int alive;
+    private int sleep;
+
+    public LP39cl(int y, int m, int d, int y2, int m2, int d2) {
+        year = y;
+        month = m;
+        day = d;
+        year2 = y2;
+        month2 = m2;
+        day2 = d2;
+        alive = 0;
+        sleep = 0;
+    }
+    public void calc() {
+        alive =
+    }
+
+}
+

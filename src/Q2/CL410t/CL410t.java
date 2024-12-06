f07365a28a857084fac086ac460d00a46586d2f4
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-12-06 11:25:07 -0600

    06.12.24

---
+++ src/Q2/CL410t/CL410t.java
@@ -0,0 +1,4 @@
+package Q2.CL410t;
+
+public class CL410t {
+}
---
+++ src/Q2/CL410t/cl410.java
@@ -0,0 +1,9 @@
+package Q2.CL410t;
+
+public class cl410 {
+    private int id;
+    private int income;
+    private int members;
+    private int average;
+    private int poverty;
+}

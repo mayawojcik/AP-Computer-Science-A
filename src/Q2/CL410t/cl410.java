695adc51d8adaad5dd7b53c322b8bd75f7f5c212
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-12-11 11:23:44 -0600

    11.12.24

--- src/Q2/CL410t/cl410.java
+++ src/Q2/CL410t/cl410.java
@@ -1,9 +1,17 @@
 package Q2.CL410t;
 
 public class cl410 {
-    private int id;
-    private int income;
-    private int members;
-    private int average;
-    private int poverty;
+    private int myId;
+    private int myIncome;
+    private int myMembers;
+    private int myAverage;
+    private int myPoverty;
+
+    public CL410t(int id, int income, int members) {
+        myId = id;
+        myIncome = income;
+        myMembers = members;
+        myAverage = 0;
+        myPoverty = 0;
+    }
 }

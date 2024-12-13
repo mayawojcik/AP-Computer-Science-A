9d944ec836ca9bb1a25acc4d351faf8fa8fb5778
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-12-13 11:24:20 -0600

    13.12.24

--- src/Q2/CL410t/CL410t.java
+++ src/Q2/CL410t/CL410t.java
@@ -1,22 +1,42 @@
 package Q2.CL410t;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class CL410t {
     public static void main(String[] args) {
         try {
-            Scanner file = new Scanner(new File("survey2.dat, survey.dat"));
+            Scanner file = new Scanner(new File("Langdat/survey2.dat"));
 
-            System.out.println("ID\tIncome\tMembers");
+            System.out.println("ID\t\tIncome\t    Members");
             while (file.hasNext()) {
+                int id = file.nextInt();
+                int in = file.nextInt();
+                int m = file.nextInt();
 
+                cl410 la = new cl410(id, in, m);
+                la.setMyMember();
+                System.out.println(la.toString());
             }
+            System.out.println("Households with income exceeding an average income of ");
 
         } catch (IOException e) {
             System.out.println("Error: " + e);
         }
     }
 }
-
+/*
+ID		Income	    Members
+1041	12180		4
+1062	13240		3
+1327	19800		2
+1483	22458		8
+1900	17000		2
+2112	18125		7
+2345	15623		2
+3210	3200		6
+3600	6500		5
+3601	11970		2
+4725	8900		3
+6217	10000		2
+9280	6200		1
+ */
--- src/Q2/CL410t/cl410.java
+++ src/Q2/CL410t/cl410.java
@@ -4,14 +4,32 @@ public class cl410 {
     private int myId;
     private int myIncome;
     private int myMembers;
-    private int myAverage;
-    private int myPoverty;
+    private double myAverage;
+    private double myPoverty;
 
-    public CL410t(int id, int income, int members) {
+    public void calc() {
+        myPoverty = 3750.00 + 750.00 * (myMembers-2);
+        myAverage = myIncome / 13.00;
+    }
+
+    public cl410(int id, int income, int members) {
         myId = id;
         myIncome = income;
         myMembers = members;
         myAverage = 0;
         myPoverty = 0;
     }
+
+    public void setMyMember() {this.calc(); }
+    public int getId() {return myId; }
+    public int getIncome() {return myIncome;}
+    public int getMembers() {return myMembers;}
+    public double getAverage() {return myAverage;}
+    public double getPoverty() {return myPoverty;}
+
+    public String toString() {
+        return myId + "\t" + myIncome + "\t\t" + myMembers;
+    }
+
+
 }

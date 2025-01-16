bb4a43d9d525ddd5ef2464c7834709eb2b461428
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-01-16 10:55:56 -0600

    16.1.25

--- src/Q2/ArrayList/MyArrayListLab.java
+++ src/Q2/ArrayList/MyArrayListLab.java
@@ -1,35 +1,80 @@
 package Q2.ArrayList;
-import java.util.*;
 
 public class MyArrayListLab {
     public static void main(String[] args) {
         IntArrayList list = new IntArrayList();
 
-        System.out.println("Testing add() and get()...");
+        System.out.println("Testing Add() and get()");
         for (int i = 0; i < 10; i++)
             list.add(i);
 
         for (int i = 0; i < list.size(); i++)
             System.out.println("list.get(" + i + ") = " + list.get(i));
 
-        System.out.println("\nTesting set()...");
+        System.out.println("\nTesting set()");
         for (int i = 0; i < list.size(); i++)
             list.set(i, i * 2);
 
         for (int i = 0; i < list.size(); i++)
             System.out.println("list.get(" + i + ") = " + list.get(i));
 
-        System.out.println("\nTesting remove()...");
+        System.out.println("\nTesting remove()");
         for (int i = 0; i < list.size(); i++)
             if (i % 3 == 0) list.remove(i);
 
         for (int i = 0; i < list.size(); i++)
             System.out.println("list.get(" + i + ") = " + list.get(i));
 
-        System.out.println("\nTesting indexOf()...");
+        System.out.println("\nTesting indexOf()");
         for (int i = 0; i < 10; i++) {
             int num = (int) (Math.random() * 20);
             System.out.println("list.indexOf(" + num + ") = " + list.indexOf(num));
         }
     }
 }
+/*
+Testing Add() and get()
+list.get(0) = 0
+list.get(1) = 1
+list.get(2) = 2
+list.get(3) = 3
+list.get(4) = 4
+list.get(5) = 5
+list.get(6) = 6
+list.get(7) = 7
+list.get(8) = 8
+list.get(9) = 9
+
+Testing set()
+list.get(0) = 0
+list.get(1) = 2
+list.get(2) = 4
+list.get(3) = 6
+list.get(4) = 8
+list.get(5) = 10
+list.get(6) = 12
+list.get(7) = 14
+list.get(8) = 16
+list.get(9) = 18
+
+Testing remove()
+list.get(0) = 2
+list.get(1) = 4
+list.get(2) = 6
+list.get(3) = 10
+list.get(4) = 12
+list.get(5) = 14
+list.get(6) = 18
+
+Testing indexOf()
+list.indexOf(8) = -1
+list.indexOf(3) = -1
+list.indexOf(17) = -1
+list.indexOf(6) = 2
+list.indexOf(19) = -1
+list.indexOf(14) = 5
+list.indexOf(14) = 5
+list.indexOf(10) = 3
+list.indexOf(15) = -1
+list.indexOf(6) = 2
+ */

faabebee1e15fcb9c2dcf104e9b8627a1068a831
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-01-16 17:25:21 -0600

    Update prog465c.java
    16.1.25

--- src/Q2/prog465c.java
+++ src/Q2/prog465c.java
@@ -1,4 +1,31 @@
 package Q2;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class prog465c {
-}
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("Langdat/prog465c.txt"));
+
+            int[][] mat = new int[input.nextInt()][input.nextInt()];
+            int totalEdges = 0;
+
+            for (int r = 0; r < mat.length; r++) {
+                for (int c = 0; c < mat[0].length; c++) {
+                    mat[r][c] = input.nextInt();
+                    if (r == 0 || r == mat.length - 1 || c == 0 || c == mat[0].length - 1) totalEdges += mat[r][c];
+                }
+            }
+
+            System.out.println("Original Table");
+            for (int r = 0; r < mat.length; r++) {
+                for (int c = 0; c < mat[0].length; c++) {
+                    System.out.print(mat[r][c] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+
+            System.out.print("The total of the edges is " + totalEdges);
+

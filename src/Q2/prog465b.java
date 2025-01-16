05ed8f7981ad59c70c4adc79957c56498c3a40cb
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-01-16 17:23:41 -0600

    16.1.25

--- src/Q2/prog465b.java
+++ src/Q2/prog465b.java
@@ -1,18 +1,48 @@
 package Q2;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class prog465b {
+public class Prog465b {
     public static void main(String[] args) {
         try {
-            Scanner file = new Scanner(new File("Langdat/prog465b.dat"));
+            Scanner input = new Scanner(new File("Langdat/prog465b.dat"));
+            int[][] mat = new int[3][4];
+            int[] arr = new int[12];
 
-            int[][] mat = new int[3][4]; //3 rows 4 columns
-            for (int r = 0; r < mat[0].length; r++)
-                for (int c = 0; c < mat[0].length; c++)
-                    mat[r][c] = file.nextInt();
+            for (int r = 0; r < mat.length; r++) {
+                for (int c = 0; c < mat[0].length; c++) {
+                    mat[r][c] = input.nextInt();
+                }
+            }
+
+            int cnt = 0;
+            for (int r = 0; r < mat.length; r++) {
+                for (int c = 0; c < mat[0].length; c++) {
+                    if (mat[r][c] < 100) {
+                        arr[cnt] = r + 1;
+                        cnt++;
+                        arr[cnt] = c + 1;
+                        cnt++;
+                    }
+                }
+            }
+
+            System.out.println("Original Table");
+            for (int r = 0; r < mat.length; r++) {
+                for (int c = 0; c < mat[0].length; c++) {
+                    System.out.print(mat[r][c] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+
+            System.out.println("Numbers Less Than 100:");
+            System.out.println("Row\t\tColumn");
+
+            for (int lcv = 0; lcv < cnt; lcv+=2) {
+                System.out.println(arr[lcv] + "\t\t" + arr[lcv+1]);
+            }
 
         } catch (IOException e) {
             System.out.println("Error: " + e);

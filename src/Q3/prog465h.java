fcfd250d512be7d8a9d5956405256e026aec533d
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-02-07 11:20:26 -0600

    07.02.25

--- src/Q3/prog465h.java
+++ src/Q3/prog465h.java
@@ -25,8 +25,8 @@ public class prog465h {
                 for (int i = 0; i < mat.length; i++)
                     for (int j = 0; j < mat[0].length; j++)
                         if (mat[i][j] != 0) {
-                            mat2[count2][0] = i++;
-                            mat2[count2][1] = j++;
+                            mat2[count2][0] = i + 1;
+                            mat2[count2][1] = j + 1;
                             mat2[count2][2] = mat[i][j];
                             count2++;
                         }
@@ -46,9 +46,9 @@ public class prog465h {
                 }
                 System.out.println();
 
-                if (count2 * 3 < row * col)
+                if ((count2 * 3) < (row * col))
                     System.out.println("The original matrix is sparse!");
-                else if (count2 * 3 > row * col)
+                else if ((count2 * 3) > (row * col))
                     System.out.println("The original matrix is abundant!!");
                 else
                     System.out.println("The original matrix and the sparse matrix are both equally efficient!!");
@@ -60,3 +60,47 @@ public class prog465h {
         }
     }
 }
+/*
+Original Matrix!!
+0	0	7	0	0	0
+0	0	0	0	-8	0
+0	0	0	0	0	0
+2	0	0	0	0	0
+0	0	0	0	0	0
+
+1	3	7
+2	5	-8
+4	1	2
+
+The original matrix is sparse!
+
+Original Matrix!!
+0	2	0	3	0	1
+8	0	4	0	1	0
+0	3	0	1	0	-7
+5	0	9	0	6	0
+0	2	0	-1	0	7
+
+
+The original matrix is abundant!!
+
+Original Matrix!!
+0	0	1	0	0	2
+3	0	0	4	0	0
+0	0	5	0	0	6
+7	0	0	8	0	0
+0	0	9	0	0	1
+
+1	3	1
+1	6	2
+2	1	3
+2	4	4
+3	3	5
+3	6	6
+4	1	7
+4	4	8
+5	3	9
+5	6	1
+
+The original matrix and the sparse matrix are both equally efficient!!
+ */

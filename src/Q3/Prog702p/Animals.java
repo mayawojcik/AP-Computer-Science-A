5095797f131a47d636c93f8d640014bdba24010e
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-03-04 11:20:38 -0600

    03.04.25

---
+++ src/Q3/Prog702p/Animals.java
@@ -0,0 +1,11 @@
+package Q3.Prog702p;
+
+public class Animals implements names {
+    private String getName;
+    private String getWord;
+
+    public Animals(String name) {
+        getName = name;
+        getWord = "";
+    }
+}

766f89cd39059eb8d22144452f80bc58b15ec7de
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-03-06 21:24:32 -0600

    Update Animals.java
    06.03.25

--- src/Q3/Prog702p/Animals.java
+++ src/Q3/Prog702p/Animals.java
@@ -8,4 +8,6 @@ public class Animals implements names {
         getName = name;
         getWord = "";
     }
+    public String getName() { return myName; }
+    public String getWord() { return myWord; }
 }

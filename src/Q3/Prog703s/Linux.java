cebef427cc59f749cf8896193e9db6c15f5bc2c0
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-03-10 11:18:28 -0500

    03.10.25

--- src/Q3/Prog703s/Linux.java
+++ src/Q3/Prog703s/Linux.java
@@ -1,4 +1,11 @@
 package Q3.Prog703s;
 
 public class Linux {
+    private int hiddenCode;
+
+    public Linux(String name, int num, double value, int hCode) {
+        super(name, num, value);
+        hiddenCode = hCode;
+    }
+    public int getHiddenCode() { return hiddenCode; }
 }
--- src/Q3/Prog703s/Windows.java
+++ src/Q3/Prog703s/Windows.java
@@ -1,4 +1,11 @@
 package Q3.Prog703s;
 
 public class Windows {
+    private double versionNumber;
+
+    public Windows(String name, int num, double val, double verNum) {
+        super(name, num, val);
+        versionNumber = verNum;
+    }
+    public double getVersionNumber() { return versionNumber; }
 }

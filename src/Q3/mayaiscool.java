43aecea8f54d2ae6c0818a95b3cac9a3f9b9a18a
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-03-12 11:22:57 -0500

    03.12.25

--- src/Q3/mayaiscool.java
+++ src/Q3/mayaiscool.java
@@ -2,6 +2,8 @@ package Q3;
 
 public class mayaiscool {
     public static void main(String[] args) {
+        System.out.println("Maya Wojcik is super cool, amazing," +
+                "and talented!");
 
     }
 }

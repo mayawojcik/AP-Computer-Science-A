821395cdef441cd8171c2243f18aec792a7692bb
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-03-13 11:23:53 -0500

    03.13.25

--- src/Q3/Prog702q/vehicle.java
+++ src/Q3/Prog702q/vehicle.java
@@ -1,17 +1,16 @@
 package Q3.Prog702q;
-
 public class vehicle {
     public String getName;
     public int getTire;
-    public double getWorth;
+    public double getValue;
 
     public vehicle(String names, int tires) {
         getName = names;
         getTire = tires;
-        getWorth = 0;
+        getValue = 0;
     }
 
     public String getName() { return getName; }
     public int getTires() { return getTire; }
-    public double getWorth() { return getWorth; }
+    public double getWorth() { return getValue; }
 }

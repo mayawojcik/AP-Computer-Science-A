62677b9c8542cc8110c39a759aa78d402f440da7
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-03-17 11:24:33 -0500

    03.17.25

--- src/Q3/Prog703s/Prog703s.java
+++ src/Q3/Prog703s/Prog703s.java
@@ -1,8 +1,4 @@
 package Q3.Prog703s;
-import Q3.Prog702q.buses;
-import Q3.Prog702q.cars;
-import Q3.Prog702q.trucks;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -20,15 +16,15 @@ public class Prog703s {
                 int num = file.nextInt();
                 double val = file.nextDouble();
                 if (type == 1) {
-                    Apple vroom = new cars(name, num, val, color);
+                    Apple vroom = new Apple(name, num, val, color);
 
                 }
                 if (type == 2) {
-                    Windows beep = new trucks(name, num, val, verNum);
+                    Windows beep = new Windows(name, num, val, verNum);
 
                 }
                 if (type == 3) {
-                    Linux round = new buses(name, num, val, hCode);
+                    Linux round = new Linux(name, num, val, hCode);
 
                 }
                 else {

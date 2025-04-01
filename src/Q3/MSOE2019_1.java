a3a5579632fdd49147beae741f201e0e83883f21
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-04-01 11:21:26 -0500

    04.01.25

--- src/Q3/MSOE2019_1.java
+++ src/Q3/MSOE2019_1.java
@@ -1,7 +1,17 @@
 package Q3;
+import java.util.Scanner;
 
 public class MSOE2019_1 {
     public static void main(String[] args) {
+        int count = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter two letters:" );
+        String letters = input.nextLine();
+        System.out.println("Enter a line of text: ");
+        String line = input.nextLine();
+        for (int o = 0; o < line.length(); o++) {
+
+        }
 
     }
 }

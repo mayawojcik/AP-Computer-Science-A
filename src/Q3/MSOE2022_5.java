18b33f5e7044da259dc15a123d10510f844874df
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-04-02 12:27:27 -0500

    04.01.25

--- src/Q3/MSOE2022_5.java
+++ src/Q3/MSOE2022_5.java
@@ -1,7 +1,22 @@
 package Q3;
+import java.util.Scanner;
 
 public class MSOE2022_5 {
     public static void main(String[] args) {
+        Scanner marlon = new Scanner(System.in);
+        int grams = 0;
+        int buckets = 0;
+        System.out.println("How many shells would you like? ");
+        int shells = marlon.nextInt();
+        for (int m = 0; m < shells; m++) {
+            System.out.println("How many grams??");
+            grams += marlon.nextInt();
+        }
+        while (grams > 0) {
+            buckets++;
+            grams -= 100;
+        }
+        System.out.println("Sally needs" + buckets + "buckets");
 
     }
 }

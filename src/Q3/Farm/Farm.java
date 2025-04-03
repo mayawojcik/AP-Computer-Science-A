0867dd266cd6eb67d0cfb6865eaae4e6b5151395
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-04-03 11:19:57 -0500

    04.03.25

---
+++ src/Q3/Farm/Farm.java
@@ -0,0 +1,11 @@
+package Q3.Farm;
+import java.util.ArrayList;
+
+public class Farm implements IFarm {
+    private ArrayList<Horse> myHorses;
+    private ArrayList<Cow> myCows;
+    private int myNumHayBales;
+    private int myNumCorn;
+    private double myHayCost;
+    private double myCornCost;
+}

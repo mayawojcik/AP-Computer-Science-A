01d0b46b9523bd1b20cbc76d36ee6352a382bad5
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-04-03 11:25:00 -0500

    04.03.25

--- src/Q3/Farm/Prog505t.java
+++ src/Q3/Farm/Prog505t.java
@@ -1,14 +1,17 @@
 package Q3.Farm;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Prog505t {
     public static void main(String[] args) {
         try {
-            Scanner file = new Scanner(new File("Langdat/prog505t.dat"));
+            Scanner sydney = new Scanner(new File("Langdat/prog505t.dat"));
+            List<Animal> animals = new ArrayList<>();
 
-            while (file.hasNext()) {
+            while (sydney.hasNext()) {
 
             }
 

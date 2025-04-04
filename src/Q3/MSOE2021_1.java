8333baaaee5953c9dbc3093eed6478ddde71eed3
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-04-04 09:03:37 -0500

    Update MSOE2021_1.java

--- src/Q3/MSOE2021_1.java
+++ src/Q3/MSOE2021_1.java
@@ -9,7 +9,19 @@ public class MSOE2021_1 {
         String letter = sydney.nextLine();
         System.out.println("NOW GIVE ME A SENTENCE GRRRRRR!!!");
         String sentence = sydney.nextLine();
-
+        for (int i = 0; i < sentence.length()-1; i++) {
+            if (sentence.substring(i, i+1).equals(" ")) {
+                if (sentence.substring(i-1, i).equals(letter)) {
+                    for (int j = i-1; j > 0; j--) {
+                        if (sentence.substring(j, j + 1).equals(" ")) {
+                            System.out.println("The word is: " + sentence.substring(j + 1, i));
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("There isn't a word ending in " + letter + "!");
+    }
 
     }
-}

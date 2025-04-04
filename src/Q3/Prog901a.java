bddea5e2cc03803fcc7a5c3d2ab13a7e40d9ff4f
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-04-04 07:29:56 -0500

    Update Prog901a.java

--- src/Q3/Prog901a.java
+++ src/Q3/Prog901a.java
@@ -1,19 +1,30 @@
 package Q3;
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Prog901a {
+    public static void printFile(Scanner sydney) {
+        String s = "";
+        while (sydney.hasNext()) {
+            s += sydney.nextLine() + "\n";
+        }
+        System.out.println(reverseString(s));
+    }
+
+    public static String rString(String s) {
+        if (s.length() == 0) return "";
+        return rString(s.substring(1)) + s.substring(0,1);
+    }
+
     public static void main(String[] args) {
         try {
             Scanner sydney = new Scanner(new File("Langdat/prog512h.dat"));
-
-            while (sydney.hasNext()) {
-
-            }
+            printFile(sydney);
 
         } catch (IOException e) {
-            System.out.println("Error: " + e);
+            System.out.println("Can't find data file!");
         }
     }
 }

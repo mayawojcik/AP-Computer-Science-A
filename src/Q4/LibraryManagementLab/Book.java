c739e4b3c67b67a985d43cbf3bf85bb1943e4724
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2025-05-08 11:05:46 -0500

    05.08.25

---
+++ src/Q4/LibraryManagementLab/Book.java
@@ -0,0 +1,18 @@
+package Q4.LibraryManagementLab;
+
+public class Book extends Media {
+    private String author;
+
+    // TODO: Implement constructor (title, isbn, author); call super constructor
+
+    public String getAuthor() { return author; }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + getTitle() + '\'' +
+                ", isbn='" + getIsbn() + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
